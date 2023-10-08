d53e5f130d41e8cdacc214d49bbf774903ea82b2
abdelrahman <zeid4643@gmail.com>
2023-10-08 14:23:32 +0300

    setup app icon and change name of app

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor =Color(0xff03174C);
+const Color kButtonColor =Color(0xff8E97FD);
+const Color kHeaderColor =Color(0xff3F414E);
+const Color kMainBGColor =Color(0xffE5E5E5);
